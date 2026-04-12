f3de817e86ff93ebd7dcb1ae828653d22546303f
Waheed <waheedneetexam@gmail.com>
2026-04-12 06:29:03 +0000

    Default RGB-Prism

--- src/utils/backend-config.ts
+++ src/utils/backend-config.ts
@@ -54,7 +54,7 @@ export const RGBITS_PRISM_API_BASE = envString(
     import.meta.env.VITE_RGBITS_PRISM_API_BASE,
     'https://prism.photonbolt.xyz'
 );
-export const DEFAULT_REGTEST_RGB_BACKEND_MODE: RegtestRgbBackendMode = 'faucet';
+export const DEFAULT_REGTEST_RGB_BACKEND_MODE: RegtestRgbBackendMode = 'prism';
 
 export const getDefaultElectrumServer = (
     network: WalletNetwork,
